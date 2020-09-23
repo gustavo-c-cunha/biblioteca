1685a04fe423831b72224d90e112559a0f54ae01
Gustavo Cunha <63239836+gustavo-c-cunha@users.noreply.github.com>
2020-09-22 21:42:45 -0300

    Adição de classes

--- inicio.py
+++ inicio.py
@@ -16,6 +16,7 @@ class Book():
         self.language = language
         self.publisher = publisher
 
+
     def getId(self):
         return self.id
 
@@ -64,4 +65,100 @@ class Book():
     def setPublisher(self, newPublisher):
         self.publisher = newPublisher
 
+class User():
+    def __init__(self, name, age, phone, email, address, cpf, admin):
+        self.name = name
+        self.age = age
+        self.phone = phone
+        self.email = email
+        self.address = address
+        self.cpf = cpf
+        self.admin = admin
         
+    def getName(self):
+        return self.name
+
+    def setName(self, newName):
+        self.name = newName
+
+    def getAge(self):
+        return self.age
+
+    def setAge(self, newAge):
+        self.age = newAge
+
+    def getPhone(self):
+        return self.phone
+
+    def setPhone(self, newPhone):
+        self.phone = newPhone
+
+    def getEmail(self):
+        return self.email
+
+    def setEmail(self, newEmail):
+        self.email = newEmail
+
+    def getAddress(self):
+        return self.address
+
+    def setAddress(self, newAddress):
+        self.address = newAddress
+    
+    def getCpf(self):
+        return self.cpf
+    
+    def setCpf(self, newCpf):
+        self.cpf = newCpf
+
+
+class Author():
+    def __init__(self, name):
+        self.name = name
+        self.books = []
+
+    def getName(self):
+        return self.name
+    
+    def setName(self, newName):
+        self.name = newName
+
+    def getBooks(self):
+        for i in range(len(self.books)):
+            return self.books[i].getTitle()
+
+    def addBook(self, book):
+        self.books.append(book)
+
+    def delBook(self, book):
+        self.books.remove(book)
+
+class Publisher():
+    def __init__(self, name, cnpj):
+        self.name = name
+        self.cnpj = cnpj
+        self.books = []
+
+    def getName(self):
+        return self.name
+
+    def setName(self, newName):
+        self.name = newName
+
+    def getCnpj(self):
+        return self.cnpj
+
+    def setCnpj(self, newCnpj):
+        self.cnpj = newCnpj
+
+    def getBooks(self):
+        for i in range(len(self.books)):
+            return self.books[i].getTitle()
+
+    def addBook(self, book):
+        self.books.append(book)
+
+    def delBook(self, book):
+        self.books.remove(book)
+
+    
